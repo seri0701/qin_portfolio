2a287d08e67c5401f6f98d1e35ea553df61bbd39
seri0701 <76086657+seri0701@users.noreply.github.com>
2022-08-18 16:34:48 +0900

    🎨 dayjsをaddして日付を追加、写真を差替え、レイアウト少し修正

--- components/Molecules/PFCard.tsx
+++ components/Molecules/PFCard.tsx
@@ -1,28 +1,32 @@
-import { FC } from 'react'
-import { Card, Grid, Group, Image, Text, Title } from '@mantine/core'
+import { FC } from "react"
+import { Card, Image, Text, Title } from "@mantine/core"
+import dayjs from "dayjs"
 
 type Props = {
   src: string
   title: string
   description: string
+  publishedAt: string
 }
 
-export const PFCard: FC<Props> = ({ src, title, description }) => {
+export const PFCard: FC<Props> = ({ src, title, description, publishedAt }) => {
+  // const publishedAt = "2022-08-05T12:48:02.516Z"
   return (
     <>
-      <Grid>
-        <Card shadow="sm" p="lg" radius="md" withBorder>
-          <Card.Section>
-            <Image src={src} height={160} alt="Norway" />
-          </Card.Section>
-          <Group position="apart" mt="md" mb="xs">
-            <Title order={5}>{title}</Title>
-          </Group>
-          <Text size="sm" color="dimmed">
-            {description}
-          </Text>
-        </Card>
-      </Grid>
+      <Card>
+        <Card.Section>
+          <Image src={src} height={160} alt="Norway" />
+        </Card.Section>
+        <Title order={5} mt="md" mb="xs">
+          {title}
+        </Title>
+        <Text size="sm" color="dimmed">
+          {description}
+        </Text>
+        <time dateTime={publishedAt} className="mt-4 block text-sm">
+          {dayjs(publishedAt).format("YYYY年MM月DD日")}
+        </time>
+      </Card>
     </>
   )
 }
--- components/Organisms/PFCardCom.tsx
+++ components/Organisms/PFCardCom.tsx
@@ -1,17 +1,18 @@
-import { FC } from 'react'
-import { Grid } from '@mantine/core'
-import { PFCard } from 'components/Molecules/PFCard'
+import { FC } from "react"
+import { Grid } from "@mantine/core"
+import { PFCard } from "components/Molecules/PFCard"
 
 export const PFCardCom: FC = () => {
   return (
     <>
-      <Grid gutter="xl">
+      <Grid gutter="xl" className="mt-4">
         {data?.map((item) => (
           <Grid.Col key={item.id} xs={12} sm={4}>
             <PFCard
               src={item.src}
               title={item.title}
               description={item.description}
+              publishedAt={item.publishedAt}
             />
           </Grid.Col>
         ))}
@@ -22,45 +23,51 @@ export const PFCardCom: FC = () => {
 
 export const data = [
   {
-    id: 'id 1',
-    src: 'vercel.svg',
-    title: 'test title 1',
+    id: "id 1",
+    src: "Thumbnail.png",
+    title: "test title 1",
     description:
-      'Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. ',
+      "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. ",
+    publishedAt: "2022-08-05T12:48:02.516Z",
   },
   {
-    id: 'id 2',
-    src: 'vercel.svg',
-    title: 'test title 2',
+    id: "id 2",
+    src: "Thumbnail.png",
+    title: "test title 2",
     description:
-      'Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. ',
+      "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. ",
+    publishedAt: "2022-08-05T12:48:02.516Z",
   },
   {
-    id: 'id 3',
-    src: 'vercel.svg',
-    title: 'test title 1',
+    id: "id 3",
+    src: "Thumbnail.png",
+    title: "test title 1",
     description:
-      'Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. ',
+      "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. ",
+    publishedAt: "2022-08-05T12:48:02.516Z",
   },
   {
-    id: 'id 4',
-    src: 'vercel.svg',
-    title: 'test title 2',
+    id: "id 4",
+    src: "Thumbnail.png",
+    title: "test title 2",
     description:
-      'Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. ',
+      "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. ",
+    publishedAt: "2022-08-05T12:48:02.516Z",
   },
   {
-    id: 'id 5',
-    src: 'vercel.svg',
-    title: 'test title 2',
+    id: "id 5",
+    src: "Thumbnail.png",
+    title: "test title 2",
     description:
-      'Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. ',
+      "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. ",
+    publishedAt: "2022-08-05T12:48:02.516Z",
   },
   {
-    id: 'id 6',
-    src: 'vercel.svg',
-    title: 'test title 2',
+    id: "id 6",
+    src: "Thumbnail.png",
+    title: "test title 2",
     description:
-      'Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. ',
+      "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. ",
+    publishedAt: "2022-08-05T12:48:02.516Z",
   },
 ]

52a891f85f7819a26b47028bdcfa8b8f3839300c
seri0701 <76086657+seri0701@users.noreply.github.com>
2022-08-27 07:03:51 +0900

    👽️ microCMSをuseQueryで取得

---
+++ hooks/useQyeryPost.tsx
@@ -0,0 +1,22 @@
+import { MicroCMSListResponse } from "microcms-js-sdk"
+import { client } from "libs/client"
+import { useQuery } from "@tanstack/react-query"
+
+export type Post = {
+  id: string
+  createdat: string
+  title: string
+  body: string
+}
+
+const getPost = async () => {
+  const data = await client.getList({ endpoint: "blog" })
+  return data
+}
+
+export const useQueryPosts = () => {
+  return useQuery<MicroCMSListResponse<Post[]>, Error>({
+    queryKey: ["posts"],
+    queryFn: getPost,
+  })
+}
